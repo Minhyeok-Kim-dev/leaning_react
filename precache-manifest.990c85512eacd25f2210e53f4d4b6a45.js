e9646ab63e6e1b12d48a973d9fdda7c63e40625c
mania125 <alsgurmaniadev@gmail.com>
2020-02-27 17:03:52 +0900

    Updates

---
+++ precache-manifest.990c85512eacd25f2210e53f4d4b6a45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c57dd4551a5bf41dca461e3b6299bb",
+    "url": "/learning_react/index.html"
+  },
+  {
+    "revision": "efda00419d6f02db6ee3",
+    "url": "/learning_react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "48fa86c15254fcb2908b",
+    "url": "/learning_react/static/js/2.69567d2c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/learning_react/static/js/2.69567d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efda00419d6f02db6ee3",
+    "url": "/learning_react/static/js/main.b31798ec.chunk.js"
+  },
+  {
+    "revision": "5541c05d59379f27b7b1",
+    "url": "/learning_react/static/js/runtime-main.a125c926.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learning_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
